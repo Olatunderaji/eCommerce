5734d4e31a78efcd4c38f3ed71a6df55f37f89d3
Your Name <olatunderaji67@gmail.com>
2023-10-07 19:30:21 +0100

    eCommerce_Edited

--- main.js
+++ main.js
@@ -29,25 +29,63 @@
 
 //Creating Elements
 
-const ul = document.querySelector('ul');
-const li = document.createElement('li');
-//To add an Element into our Document is using the Append
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+// //To add an Element into our Document is using the Append
 
-ul.append(li)
+// ul.append(li)
 
-//Modifying the text
-
-
-const firstListItem = document.querySelector('.list-items');
-
-li.innerText = 'X-Men';
-
-//Modifying Attributes & Classes
-li.setAttribute('id', 'main-heading')
-li.removeAttribute(id)
+// //Modifying the text
 
 
-const title = document.querySelector('#main-heading')
+// const firstListItem = document.querySelector('.list-items');
+
+// li.innerText = 'X-Men';
+
+// //Modifying Attributes & Classes
+// li.setAttribute('id', 'main-heading')
+// li.removeAttribute(id)
+
+
+// const title = document.querySelector('#main-heading')
+
+// Using the properties of Classlist and also add methods to it
+//Grab the styling of the other List-Items using the .add
+// li.classList.add('list-items') //or to remove using the .remove method
+
+// // to find out if that element contains the properties of specific class
+// console.log(li.classList.contains("list-items"));
+
+// // Remove Elements
+// li.remove();
+
+
+// How to navigate the Dom
+//Traverse the DOM
+//Parent Node Traversal
+// let ul = document.querySelector('ul')
+// console.log (ul.parentNode.parentNode);
+// console.log (ul.parentElement);
+
+// const html = document.documentElement;
+// console.log(html)
+
+
+//Child Node Traversal
+let ul = document.querySelector('ul')
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+
+// ul.childNodes[9].style.backgroundColor = 'yellow'
+
+console.log(ul.children)
+console.log(ul.firstElementChild)
+console.log(ul.lastElementChild)
+
+//Sibling Node Traversal*/
+
+
 
 
 
